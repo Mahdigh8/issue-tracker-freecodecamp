5637d3e03b950ff2e45153984cdfc6f777c682f5
mahdi ghazaei <mahdighazaei8@gmail.com>
2022-10-25 12:34:36 +0000

    feat: add post & get method handlers

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,54 @@
-'use strict';
+"use strict";
+
+const Project = require("../models/project");
+const Issue = require("../models/issue");
 
 module.exports = function (app) {
+  app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.url}`);
+  });
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+  app
+    .route("/api/issues/:project")
+
+    .get(async function (req, res) {
+      let projectName = req.params.project;
+      const project = await Project.findOne({ name: projectName });
+      if (project) return res.send([]);
+      const allIssues = await Issue.find({ project_id: project._id });
+      res.send(allIssues);
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(async function (req, res) {
+      let projectName = req.params.project;
+      const { issue_title, issue_text, created_by } = req.body;
+      const { assigned_to, status_text } = req.body;
+
+      if (!(issue_title && issue_text && created_by)) {
+        return res.send({ error: "required field(s) missing" });
+      }
+
+      let project = await Project.findOne({ name: projectName });
+      if (!project) {
+        project = await Project.create({ name: projectName });
+      }
+
+      const newIssue = await Issue.create({
+        project_id: project._id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to: assigned_to || "",
+        status_text: status_text || "",
+      });
+      res.status(201).send(newIssue);
     })
-    
-    .put(function (req, res){
+
+    .put(async function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(async function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
